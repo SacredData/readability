f5e6a2844df45b56e0cc79a07f0b4b8082411234
agrathwohl <agrathwohl@protonmail.ch>
2015-11-17 16:23:45 -0500

    added Article() functionality from newspaper class; simplified output; added approximate duration of spoken recording; added NLP functionality from newspaper class

--- readability.py
+++ readability.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 
 import math
+import argparse
 
 from utils import get_char_count
 from utils import get_words
 from utils import get_sentences
 from utils import count_syllables
 from utils import count_complex_words
+from newspaper import Article
 
 
 class Readability:
+    """
+    Pass a URL, get a bunch of analysis of the text held within it.
+    The analysis will focus primarily upon the text's complexity and
+    its other features relevant to the production of a spoken audio version.
+    Essentially, this class will be used to populate a few records
+    on the production db of clients who work with text content.
+
+    This code is the result of a fork of the awesome Readability
+    repo by Max Mautner. Thanks, my man!
+    https://github.com/mmautner/readability
+    """
     analyzedVars = {}
 
     def __init__(self, text):
@@ -23,7 +36,7 @@ class Readability:
         syllable_count = count_syllables(words)
         complexwords_count = count_complex_words(text)
         avg_words_p_sentence = word_count/sentence_count
-        
+
         self.analyzedVars = {
             'words': words,
             'char_cnt': float(char_count),
@@ -35,44 +48,44 @@ class Readability:
         }
 
     def ARI(self):
-        score = 0.0 
+        score = 0.0
         if self.analyzedVars['word_cnt'] > 0.0:
             score = 4.71 * (self.analyzedVars['char_cnt'] / self.analyzedVars['word_cnt']) + 0.5 * (self.analyzedVars['word_cnt'] / self.analyzedVars['sentence_cnt']) - 21.43
         return score
-        
+
     def FleschReadingEase(self):
-        score = 0.0 
+        score = 0.0
         if self.analyzedVars['word_cnt'] > 0.0:
             score = 206.835 - (1.015 * (self.analyzedVars['avg_words_p_sentence'])) - (84.6 * (self.analyzedVars['syllable_cnt']/ self.analyzedVars['word_cnt']))
         return round(score, 4)
-        
+
     def FleschKincaidGradeLevel(self):
-        score = 0.0 
+        score = 0.0
         if self.analyzedVars['word_cnt'] > 0.0:
             score = 0.39 * (self.analyzedVars['avg_words_p_sentence']) + 11.8 * (self.analyzedVars['syllable_cnt']/ self.analyzedVars['word_cnt']) - 15.59
         return round(score, 4)
-        
+
     def GunningFogIndex(self):
-        score = 0.0 
+        score = 0.0
         if self.analyzedVars['word_cnt'] > 0.0:
             score = 0.4 * ((self.analyzedVars['avg_words_p_sentence']) + (100 * (self.analyzedVars['complex_word_cnt']/self.analyzedVars['word_cnt'])))
         return round(score, 4)
 
     def SMOGIndex(self):
-        score = 0.0 
+        score = 0.0
         if self.analyzedVars['word_cnt'] > 0.0:
             score = (math.sqrt(self.analyzedVars['complex_word_cnt']*(30/self.analyzedVars['sentence_cnt'])) + 3)
         return score
 
     def ColemanLiauIndex(self):
-        score = 0.0 
+        score = 0.0
         if self.analyzedVars['word_cnt'] > 0.0:
             score = (5.89*(self.analyzedVars['char_cnt']/self.analyzedVars['word_cnt']))-(30*(self.analyzedVars['sentence_cnt']/self.analyzedVars['word_cnt']))-15.8
         return round(score, 4)
 
     def LIX(self):
         longwords = 0.0
-        score = 0.0 
+        score = 0.0
         if self.analyzedVars['word_cnt'] > 0.0:
             for word in self.analyzedVars['words']:
                 if len(word) >= 7:
@@ -82,27 +95,50 @@ class Readability:
 
     def RIX(self):
         longwords = 0.0
-        score = 0.0 
+        score = 0.0
         if self.analyzedVars['word_cnt'] > 0.0:
             for word in self.analyzedVars['words']:
                 if len(word) >= 7:
                     longwords += 1.0
             score = longwords / self.analyzedVars['sentence_cnt']
         return score
-        
+
 
 if __name__ == "__main__":
-    text = """We are close to wrapping up our 10 week Rails Course. This week we will cover a handful of topics commonly encountered in Rails projects. We then wrap up with part 2 of our Reddit on Rails exercise!  By now you should be hard at work on your personal projects. The students in the course just presented in front of the class with some live demos and a brief intro to to the problems their app were solving. Maybe set aside some time this week to show someone your progress, block off 5 minutes and describe what goal you are working towards, the current state of the project (is it almost done, just getting started, needs UI, etc.), and then show them a quick demo of the app. Explain what type of feedback you are looking for (conceptual, design, usability, etc.) and see what they have to say.  As we are wrapping up the course you need to be focused on learning as much as you can, but also making sure you have the tools to succeed after the class is over."""
-
-    rd = Readability(text)
-    print 'Test text:'
-    print '"%s"\n' % text
-    print 'ARI: ', rd.ARI()
-    print 'FleschReadingEase: ', rd.FleschReadingEase()
-    print 'FleschKincaidGradeLevel: ', rd.FleschKincaidGradeLevel()
-    print 'GunningFogIndex: ', rd.GunningFogIndex()
-    print 'SMOGIndex: ', rd.SMOGIndex()
-    print 'ColemanLiauIndex: ', rd.ColemanLiauIndex()
-    print 'LIX: ', rd.LIX()
-    print 'RIX: ', rd.RIX()
-
+    parser = argparse.ArgumentParser(description='Text analysis via URL.')
+    parser.add_argument('-url', type=str, help='URL of Article')
+    args = parser.parse_args()
+    url = args.url
+    a = Article(url)
+    print 'Retrieving article at URL: ', url
+    a.download()
+    print 'Article downloaded.'
+    a.parse()
+    print 'Article parsed.'
+    text = a.text
+    a.nlp()
+    title = a.title
+    summary = a.summary
+    keywords = a.keywords
+    seo_terms = []
+    for word in keywords:
+        seo_terms.append(str(word))
+    print 'Article title:    ', title
+    # print 'Article summary:  ', summary
+    print 'Article text:     ', text
+    rd = Readability(str(text))
+    spoken_dur = rd.analyzedVars['word_cnt'] / 2.5 / 60  # approx minutes
+    spoken_dur = round(spoken_dur, 2)
+    if rd.SMOGIndex() > 12.0:
+        read_level = 'College'
+    elif rd.SMOGIndex() > 8.0:
+        read_level = 'High school'
+    elif rd.SMOGIndex() > 5.0:
+        read_level = 'Middle school'
+    else:
+        read_level = 'Elementary school'
+    print ''
+    print 'SMOG Readability score:  ', rd.SMOGIndex()
+    print 'Reading grade level:     ', read_level
+    print 'Approx. spoken duration: ', str(spoken_dur), ' minutes'
+    print 'Article keywords:        ', seo_terms
